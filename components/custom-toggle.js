11e148eecd1b9f0a8b7e6dc7f3dc83a9048ae0bb
cagloria <33134245+cagloria@users.noreply.github.com>
2020-03-24 18:45:14 -0700

    Adjusted scss files
    - Imported scss files into main styles file
    - Moved main script into scripts folder

--- components/custom-toggle.js
+++ components/custom-toggle.js
@@ -1,5 +1,3 @@
-"use strict";
-
 class CustomToggle extends HTMLElement {
     constructor() {
         super();
--- components/pricing-panel.js
+++ components/pricing-panel.js
@@ -1,5 +1,3 @@
-"use strict";
-
 class PricingPanel extends HTMLElement {
     constructor() {
         super();
