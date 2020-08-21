91a6858120220d70a75222a6d763623267d66004
cagloria <33134245+cagloria@users.noreply.github.com>
2020-08-21 13:24:18 -0700

    Renamed SCSS variables and pricing panel classes

--- components/pricing-panel.js
+++ components/pricing-panel.js
@@ -12,20 +12,18 @@ class PricingPanel extends HTMLLIElement {
 
         this.innerHTML = `
             <p class="pricing-panel__title">${name}</p>
+
             <p class="pricing-panel__price">
                 <span class="pricing-panel__price-dollar">$</span>
                 <span class="pricing-panel__price-numbers">${price}</span>
             </p>
-            <ul class="pricing-panel__benefits-ul">
-                <li class="pricing-panel__benefit">${storage} Storage</li>
-                <li class="pricing-panel__benefit">
-                    ${userLimit} Users Allowed
-                </li>
-                <li class="pricing-panel__benefit">
-                    Send up to ${sendingLimit}
-                </li>
+            
+            <ul class="pricing-panel__benefits">
+                <li>${storage} Storage</li>
+                <li>${userLimit} Users Allowed</li>
+                <li>Send up to ${sendingLimit}</li>
             </ul>
-        
+            
             <button class="button pricing-panel__button">Learn More</button>
         `;
     }
